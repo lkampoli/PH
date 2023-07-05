530d6182bb5cb76bdec43ae2b8b2a519095fe46b
Lorenzo <lorenzo.campoli@unimelb.edu.au>
2023-07-05 19:03:01 +1000

    update line cluster for PH

---
+++ 05_Results/extract_columns.sh
@@ -0,0 +1,32 @@
+awk '{ print $1,$2,$3 }' < X0.05_c0.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X0.05_c0.xy
+awk '{ print $1,$2,$3 }' < X0.5_c0.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X0.5_c0.xy
+awk '{ print $1,$2,$3 }' < X1.0_c0.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X1.0_c0.xy
+awk '{ print $1,$2,$3 }' < X2.0_c0.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X2.0_c0.xy
+awk '{ print $1,$2,$3 }' < X3.0_c0.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X3.0_c0.xy
+awk '{ print $1,$2,$3 }' < X4.0_c0.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X4.0_c0.xy
+awk '{ print $1,$2,$3 }' < X5.0_c0.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X5.0_c0.xy
+awk '{ print $1,$2,$3 }' < X6.0_c0.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X6.0_c0.xy
+awk '{ print $1,$2,$3 }' < X7.0_c0.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X7.0_c0.xy
+awk '{ print $1,$2,$3 }' < X8.0_c0.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X8.0_c0.xy
+
+awk '{ print $1,$2,$3 }' < X0.05_c1.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X0.05_c1.xy
+awk '{ print $1,$2,$3 }' < X0.5_c1.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X0.5_c1.xy
+awk '{ print $1,$2,$3 }' < X1.0_c1.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X1.0_c1.xy
+awk '{ print $1,$2,$3 }' < X2.0_c1.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X2.0_c1.xy
+awk '{ print $1,$2,$3 }' < X3.0_c1.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X3.0_c1.xy
+awk '{ print $1,$2,$3 }' < X4.0_c1.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X4.0_c1.xy
+awk '{ print $1,$2,$3 }' < X5.0_c1.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X5.0_c1.xy
+awk '{ print $1,$2,$3 }' < X6.0_c1.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X6.0_c1.xy
+awk '{ print $1,$2,$3 }' < X7.0_c1.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X7.0_c1.xy
+awk '{ print $1,$2,$3 }' < X8.0_c1.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X8.0_c1.xy
+
+awk '{ print $1,$2,$3 }' < X0.05_c2.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X0.05_c2.xy
+awk '{ print $1,$2,$3 }' < X0.5_c2.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X0.5_c2.xy
+awk '{ print $1,$2,$3 }' < X1.0_c2.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X1.0_c2.xy
+awk '{ print $1,$2,$3 }' < X2.0_c2.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X2.0_c2.xy
+awk '{ print $1,$2,$3 }' < X3.0_c2.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X3.0_c2.xy
+awk '{ print $1,$2,$3 }' < X4.0_c2.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X4.0_c2.xy
+awk '{ print $1,$2,$3 }' < X5.0_c2.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X5.0_c2.xy
+awk '{ print $1,$2,$3 }' < X6.0_c2.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X6.0_c2.xy
+awk '{ print $1,$2,$3 }' < X7.0_c2.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X7.0_c2.xy
+awk '{ print $1,$2,$3 }' < X8.0_c2.txt > outfile; sed 's/\(.*\)/\(\1\)/' outfile > X8.0_c2.xy
